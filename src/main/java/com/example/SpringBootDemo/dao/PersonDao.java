e7ec18438a76a1bfbd59aee19e6013c4840ea17b
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-15 22:00:39 +0100

    added DAO to project

--- src/main/java/com/example/SpringBootDemo/dao/PersonDao.java
+++ src/main/java/com/example/SpringBootDemo/dao/PersonDao.java
@@ -1,2 +1,16 @@
-package com.example.SpringBootDemo.dao;public interface PersonDao {
+package com.example.SpringBootDemo.dao;
+
+import com.example.SpringBootDemo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person) {
+        UUID id = UUID.randomUUID();
+
+        return insertPerson(id, person);
+    }
 }
